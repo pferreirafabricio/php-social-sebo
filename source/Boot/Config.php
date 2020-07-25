2f3b7dffbdc88a9f6712382a6d2dbb1389235687
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-07-25 15:52:29 -0300

    add get and post helpers methods and documentation

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -1,6 +1,37 @@
 <?php 
 date_default_timezone_set('America/Sao_Paulo');
+
+/**
+ * The default filter for date time
+ */
 define('DATE_TIME', 'd/m/Y H:i:s');
 
+/**
+ * The base directory of the project
+ */
 define('BASE', '/php-socialSebo/');
+
+/**
+ * The host of the project
+ */
 define('HOST', 'http://localhost/php-socialSebo/');
+
+/**
+ * The host of the database
+ */
+define('DB_HOST', '127.0.0.1');
+
+/**
+ * The user for access database
+ */
+define('DB_USER', 'root');
+
+/**
+ * The password for access database
+ */
+define('DB_PASSWORD', '');
+
+/**
+ * The schema name in database of the project
+ */
+define('DB_SCHEMA', 'social-sebo');
--- source/Boot/Helpers.php
+++ source/Boot/Helpers.php
@@ -1,9 +1,57 @@
 <?php
 
-function dd($param = [])
+/**
+ * Format and dump a variable and die
+ * 
+ * @param $variable Variable to be formated
+ */
+function dd($variable = [])
 {
     echo '<pre>';
-    print_r($param);
+    print_r($variable);
     echo '</pre>';
     die();
+}
+
+/**
+ * Gets the query string data
+ *
+ * @param string $param The param to get in URL.
+ * @param $filter The filter to be applied in the param.
+ */
+function get(string $param, $filter = FILTER_SANITIZE_STRING)
+{
+    return filter_input(INPUT_GET, $param, $filter);
+}
+
+/**
+ * Gets the body data
+ *
+ * @param string $param The param to get in body request.
+ * @param $filter The filter to be applied in the param.
+ */
+function post(string $param, $filter = FILTER_SANITIZE_STRING) 
+{
+    return filter_input(INPUT_POST, $param, $filter);
+}
+
+/**
+ * Gets the current date
+ * 
+ * @param string $dateFormat Format to date be parsed
+ * @return DateTime
+ */
+function getCurrentDate(string $dateFormat = 'Y-m-d H:i:s')
+{
+    return date($dateFormat);
+}
+
+/**
+ * Redirect the user to a specific URL
+ * 
+ * @param string $url URL to which the user will be redirected to
+ */
+function redirect(string $url)
+{
+    header('Location: ' . $url);
 }
