216c0117b8982ce573fc8dee20500506ec2a6df8
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-07-25 22:24:24 -0300

    add Bootstrap framework

--- views/partials/main.twig.php
+++ views/partials/main.twig.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{% block title %}{% endblock %} | Social Sebo</title>
+    <link rel="stylesheet" href="{{BASE}}/assets/css/bootstrap.min.css" />
 </head>
 <body>
     {% include 'partials/header.twig.php' %}
